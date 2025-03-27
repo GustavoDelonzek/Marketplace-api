8e9bbe7a512fe43b5db5c1c4c2a7f8557353e9ed
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-03-26 23:57:03 -0300

    Cria model cartItem e define relacionamentos

--- src/app/Models/CartItem.php
+++ src/app/Models/CartItem.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class CartItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'cart_id',
+        'product_id',
+        'quantity',
+        'unit_price'
+    ];
+
+    public function cart():BelongsTo
+    {
+        return $this->belongsTo(Cart::class);
+    }
+
+    public function product(): HasOne
+    {
+        return $this->hasOne(Product::class);
+    }
 }
