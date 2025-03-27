4c85ab470423e90e39f2a03133506fcdd349e72c
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-03-27 00:03:01 -0300

    Cria model coupon e define relacionamentos

--- src/app/Models/Coupon.php
+++ src/app/Models/Coupon.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Coupon extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable =[
+        'code',
+        'start_date',
+        'end_date',
+        'discount_percentage'
+    ];
+
+    public function orders():BelongsToMany
+    {
+        return $this->belongsToMany(Order::class);
+    }
+
+    
 }
