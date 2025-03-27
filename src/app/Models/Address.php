7de3c2d1362b5d51e5d1ca1e44a683f0e3230aaa
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-03-27 00:00:39 -0300

    Cria model address e define relacionamentos

--- src/app/Models/Address.php
+++ src/app/Models/Address.php
@@ -4,8 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Address extends Model
 {
-    use HasFactory, sof;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'street',
+        'number',
+        'zip',
+        'city',
+        'state',
+        'city',
+    ];
+
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
+    
 }
