dc71e6fb7ed5c704aa8c6b4a8cb84130715145d3
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-03-26 23:49:04 -0300

    Cria model discount e define relacionamentos

--- src/app/Models/Discount.php
+++ src/app/Models/Discount.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Discount extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'description',
+        'start_date',
+        'end_date',
+        'discount_percentage',
+    ];
+
+    public function products():BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
