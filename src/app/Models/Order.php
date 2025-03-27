28834382b05055f2ecb3924b3cd41f8b38db5d5d
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-03-27 00:13:08 -0300

    Cria model order e define relacionamentos

--- src/app/Models/Order.php
+++ src/app/Models/Order.php
@@ -4,8 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'address_id',
+        'order_date',
+        'coupon_id',
+        'status',
+        'total_amount',
+    ];
+
+    public function address(): BelongsTo
+    {
+        return $this->belongsTo(Address::class);
+    }
+
+    public function coupon(): HasOne
+    {
+        return $this->hasOne(Coupon::class);
+    }
 }
