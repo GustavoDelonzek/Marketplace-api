7dd65920551c90a320f4bcd7efac507ad19d5e61
GustavoDelonzek <gustavo2016delonzek@gmail.com>
2025-04-15 14:17:56 -0300

    Criando factory e seeder para as models category e product

---
+++ src/database/factories/CategoryFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "name" => $this->faker->name,
+            "description" => $this->faker->sentence,
+            "image_path" => 'public/categories/defaultCategory.png',
+        ];
+    }
+}
---
+++ src/database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'category_id' => $this->faker->randomElement(Category::all()->pluck('id')->toArray()),
+            'stock' => $this->faker->numberBetween(1, 100),
+            'price' => $this->faker->numberBetween(1, 1000),
+            'image_path' => 'public/products/defaultProduct.png',
+        ];
+    }
+}
